f1f155ba5a9baf8a6a863e6a9b2caf74368a06c4
Matthew Cline <mdeancline@gmail.com>
2026-05-16 20:51:33 -0500

    Fix class exports incorrectly declared as type-only

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { hldocument } from './internal/high-level-document-impl';
-export type { default as HighLevelDocument, HighLevelDocumentEventMap } from './elements/high-level-document';
-export type { default as HighLevelElement } from './elements/high-level-element';
-export type { default as Accordion, AccordionEventMap } from './elements/accordion';
-export type { default as OrderForm, OrderFormEventMap, StripeElementTypeMap } from './elements/order-form';
-export type { default as OrderBump } from './elements/order-bump';
+export { default as HighLevelDocument, HighLevelDocumentEventMap } from './elements/high-level-document';
+export { default as HighLevelElement } from './elements/high-level-element';
+export { default as Accordion, AccordionEventMap } from './elements/accordion';
+export { default as OrderForm, OrderFormEventMap, StripeElementTypeMap } from './elements/order-form';
+export { default as OrderBump, OrderBumpEventMap } from './elements/order-bump';
