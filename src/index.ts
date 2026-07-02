b39490c1138657c342f6a1bbeb942e77767af456
Matthew Cline <mdeancline@gmail.com>
2026-07-02 16:19:41 -0500

    feat: export missing public API types from entry point

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ export { hldocument } from './internal/real-high-level-document';
 export { GHLElementsError } from './api/ghl-elements-error';
 export { HighLevelDocument, HighLevelDocumentEventMap } from './api/high-level-document';
 export { HighLevelElement } from './api/high-level-element';
-export { KeyboardAccessibleElement } from './api/keyboard-accessible-element';
-export { Accordion, AccordionEventMap } from './api/accordion';
-export { OrderForm, OrderFormEventMap } from './api/order-form';
-export { OrderBump, OrderBumpEventMap } from './api/order-bump';
+export { KeyboardAccessibleElement, KeyboardEventMap, KeyboardActionDetails } from './api/keyboard-accessible-element';
+export { Accordion, AccordionEventMap, AccordionInteractionDetails } from './api/accordion';
+export { OrderForm, OrderFormEventMap, CouponUsageDetails } from './api/order-form';
+export { OrderBump, OrderBumpEventMap, OrderBumpSelectionDetails } from './api/order-bump';
 // TODO create custom console logging formatter
